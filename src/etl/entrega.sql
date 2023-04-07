c15cb5d59bc311ddfa70ccc0c4b5e08badbebccb
leniroperretto@gmail.com <leniroperretto@gmail.com>
2023-04-07 15:57:49 +0000

    Fix

--- src/etl/entrega.sql
+++ src/etl/entrega.sql
@@ -17,6 +17,7 @@ ON t1.idPedido = t2.idPedido
 
 WHERE dtPedido < '2018-01-01'
 AND dtPedido >= add_months('2018-01-01', -6)
+AND idVendedor IS NOT NULL
 
 GROUP BY t1.idPedido,
        t2.idVendedor,
@@ -27,7 +28,8 @@ GROUP BY t1.idPedido,
        t1.dtEstimativaEntrega
 )
 
-SELECT idVendedor,
+SELECT '2018-01-01' AS dtReference,
+       idVendedor,
        COUNT(CASE WHEN DATE(COALESCE(dtEntregue, '2018-01-01')) > DATE(dtEstimativaEntrega) THEN idPedido END) / 
                        COUNT(DISTINCT CASE WHEN descSituacao = 'delivered' THEN idPedido END) AS pctPedidoAtraso,
        COUNT(DISTINCT CASE WHEN descSituacao = 'canceled' THEN idPedido END) / 
@@ -41,4 +43,4 @@ SELECT idVendedor,
        AVG(DATEDIFF(dtEstimativaEntrega, COALESCE(dtEntregue, '2018-01-01'))) AS qtdeDiasEntregaPromessa
 
 FROM tb_pedido
-GROUP BY 1
+GROUP BY 2
